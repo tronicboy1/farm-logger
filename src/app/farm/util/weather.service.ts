0011d0f30284ecad8bbb23346a49c15d94809a55
tronicboy1 <mayeraus@icloud.com>
2022-11-28 07:40:30 +0900

    get weather service working

--- src/app/farm/util/weather.service.ts
+++ src/app/farm/util/weather.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from "@angular/core";
+import { from, map, switchMap } from "rxjs";
+import { GeolocationService } from "./geolocation.service";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root",
 })
 export class WeatherService {
-  constructor() {}
+  static key = "a4d7ce1f7e70a4cebb0c4b9c4880c42d";
+
+  constructor(private geolocationService: GeolocationService, private http: HttpClient) {}
+
+  public getWeatherReport() {
+    return from(this.geolocationService.aquireLocation(true)).pipe(
+      switchMap(([lat, lon]) => {
+        const url = new URL("https://api.openweathermap.org/data/3.0/onecall");
+        url.searchParams.set("lat", String(lat));
+        url.searchParams.set("lon", String(lon));
+        url.searchParams.set("appid", WeatherService.key);
+        url.searchParams.set("units", "metric");
+        url.searchParams.set("lang", "ja");
+        return this.http.get(URL.toString(), { observe: "body", responseType: "json" });
+      }),
+      //map((results: unknown) => )
+    );
+  }
 }
