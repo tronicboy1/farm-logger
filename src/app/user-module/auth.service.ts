90567ebd86a98150c95cdea877145eee5e01d8e9
tronicboy1 <mayeraus@icloud.com>
2022-11-28 20:16:18 +0900

    make auth state a singleton

--- src/app/user-module/auth.service.ts
+++ src/app/user-module/auth.service.ts
@@ -12,7 +12,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
-import { filter, map, Observable } from "rxjs";
+import { filter, map, Observable, shareReplay } from "rxjs";
 import type { OperatorFunction } from "rxjs";
 import type { User } from "firebase/auth";
 import { FirebaseAuth } from "@custom-firebase/inheritables/auth";
@@ -26,7 +26,7 @@ export type FilteredAuthState = OperatorFunction<User | null, User>;
   providedIn: UserModule,
 })
 export class AuthService extends FirebaseAuth {
-  public user: User | null = null;
+  private authState$?: Observable<User | null>;
 
   constructor(private userService: UserService) {
     super();
@@ -34,16 +34,14 @@ export class AuthService extends FirebaseAuth {
 
   public createUser(email: string, password: string) {
     return createUserWithEmailAndPassword(this.auth, email, password).then((creds) => {
-      this.user = creds.user;
-      return this.userService.setUidRecord(email, this.user.uid);
+      return this.userService.setUidRecord(email, creds.user.uid);
     });
   }
 
   public signInUser(email: string, password: string) {
     return signInWithEmailAndPassword(this.auth, email, password).then((creds) => {
-      this.user = creds.user;
       return this.userService
-        .setUidRecord(email, this.user.uid)
+        .setUidRecord(email, creds.user.uid)
         .then(() => this.userService.setOnlineStatus(creds.user.uid, "online"));
     });
   }
@@ -93,13 +91,12 @@ export class AuthService extends FirebaseAuth {
   }
 
   public getAuthState() {
-    return new Observable<User | null>((observer) => {
+    return (this.authState$ ||= new Observable<User | null>((observer) => {
       let unsubscribe = onAuthStateChanged(this.auth, (user) => {
-        this.user = user;
         observer.next(user);
       });
       return unsubscribe;
-    });
+    }).pipe(shareReplay(1)));
   }
 
   /**
