61c72f01704628606197560062968458b5d6d95a
tronicboy1 <mayeraus@icloud.com>
2022-12-01 10:00:11 +0900

    move nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ComponentsModule } from "./components/components.module";
 import { FarmModule } from "./farm/farm.module";
-import { NavBarModule } from "./nav-bar/nav-bar.module";
 import { PagesModule } from "./pages/pages.module";
 import { UserModule } from "./user-module/user.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule, PagesModule, NavBarModule, FarmModule],
+  imports: [BrowserModule, AppRoutingModule, UserModule, PagesModule, FarmModule, ComponentsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LocationComponent } from "./location/location.component";
+import { NavBarModule } from "./nav-bar/nav-bar.module";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @NgModule({
   declarations: [LocationComponent],
-  imports: [CommonModule],
-  exports: [LocationComponent]
+  imports: [CommonModule, NavBarModule],
+  exports: [LocationComponent, NavBarComponent]
 })
 export class ComponentsModule {}
