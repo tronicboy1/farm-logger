9290d3e728b0d7a444fc960c4f59c7bc3646f9b7
tronicboy1 <mayeraus@icloud.com>
2022-12-02 11:59:09 +0900

    optimize home

--- src/app/pages/farm/farm.component.ts
+++ src/app/pages/farm/farm.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Subscription, switchMap } from "rxjs";
+import { Observable, switchMap } from "rxjs";
 import { Farm } from "src/app/farm/farm.model";
 import { FarmService } from "src/app/farm/farm.service";
 
@@ -9,30 +9,18 @@ import { FarmService } from "src/app/farm/farm.service";
   templateUrl: "./farm.component.html",
   styleUrls: ["./farm.component.css"],
 })
-export class FarmComponent implements OnInit, OnDestroy {
-  public farm?: Farm;
-
-  private subscriptions: Subscription[] = [];
+export class FarmComponent implements OnInit {
+  public farm = new Observable<Farm>();
 
   constructor(private route: ActivatedRoute, private farmService: FarmService) {}
 
   ngOnInit(): void {
-    this.subscriptions.push(
-      this.route.params
-        .pipe(
-          switchMap((params) => {
-            const { farmId } = params;
-            if (typeof farmId !== "string") throw TypeError("Farm ID was not in params.");
-            return this.farmService.watchFarm(farmId);
-          }),
-        )
-        .subscribe((farm) => {
-          this.farm = farm;
-        }),
+    this.farm = this.route.params.pipe(
+      switchMap((params) => {
+        const { farmId } = params;
+        if (typeof farmId !== "string") throw TypeError("Farm ID was not in params.");
+        return this.farmService.watchFarm(farmId);
+      }),
     );
-  }
-
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((sub) => sub.unsubscribe());
   }
 }
