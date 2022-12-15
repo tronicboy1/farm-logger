e2d0efacb4162fbc7b584dc998e9a0a1c30cb701
tronicboy1 <mayeraus@icloud.com>
2022-12-16 07:11:59 +0900

    set global locale to japanese

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { LOCALE_ID, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -7,11 +7,15 @@ import { ComponentsModule } from "./components/components.module";
 import { FarmModule } from "./farm/farm.module";
 import { PagesModule } from "./pages/pages.module";
 import { UserModule } from "./user-module/user.module";
+import jaLocale from "@angular/common/locales/ja"
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(jaLocale);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, UserModule, PagesModule, FarmModule, ComponentsModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: "ja-JP" }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
