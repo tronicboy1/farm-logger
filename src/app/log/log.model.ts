f2e67099e52636a22fab1fc15b02d3e00e7d83b0
tronicboy1 <mayeraus@icloud.com>
2022-12-17 07:11:11 +0900

    get log service working

---
+++ src/app/log/log.model.ts
@@ -0,0 +1,53 @@
+export type LogEntry = {
+  uid: string;
+  createdAt: number;
+  action: number;
+  iconCode?: number;
+  value: string;
+}
+
+export type RenderedLogEntry = {
+  createdAt: number,
+  message: string;
+}
+
+export enum LogActions {
+  CreateFarm,
+  ViewFarm,
+  AddMember,
+  DeleteMember,
+  AddArea,
+  ViewArea,
+  AddWeatherData,
+  AddAreaLocation,
+  AddTree,
+  AddTreeReport,
+  DeleteTreeReport,
+  AddFertilization,
+  DeleteFertilization,
+  AddCropdust,
+  DeleteCropdust
+}
+
+export enum LogIconCodes {
+  Delete,
+  Add,
+  View
+}
+
+export const logDictionary: Record<number, string> = {
+  [LogActions.CreateFarm]: "がこの農場を登録した",
+  [LogActions.ViewFarm]: "がこの農場を閲覧した",
+  [LogActions.AddMember]: "がメンバーを追加した",
+  [LogActions.DeleteMember]: "がメンバーを削除した",
+  [LogActions.AddArea]: "が区域を追加した：",
+  [LogActions.AddWeatherData]: "が気象データを記録した",
+  [LogActions.AddAreaLocation]: "が区域の位置情報を記録した",
+  [LogActions.AddTree]: "が木を新たに登録した：",
+  [LogActions.AddTreeReport]: "が木の育成記録を作成した：",
+  [LogActions.DeleteTreeReport]: "が木の育成記録を削除した",
+  [LogActions.AddFertilization]: "が施肥記録を作成した",
+  [LogActions.DeleteFertilization]: "が施肥記録を削除した",
+  [LogActions.AddCropdust]: "がこの農場を登録した。",
+  [LogActions.DeleteCropdust]: "がこの農場を登録した。",
+}
--- src/app/log/log.service.ts
+++ src/app/log/log.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from "@angular/core";
+import { Firebase } from "@custom-firebase/index";
+import { AuthService } from "@user/auth.service";
+import { UserData, UserService } from "@user/user.service";
+import { collection, getDocs, query, limit, orderBy, addDoc } from "firebase/firestore";
+import { first, forkJoin, from, map, Observable, switchMap } from "rxjs";
+import { logDictionary, LogEntry, RenderedLogEntry } from "./log.model";
 import { LogModule } from "./log.module";
 
 @Injectable({
   providedIn: LogModule,
 })
 export class LogService {
-  constructor() {}
+  static path = "logs";
+  private logsCache$?: Observable<RenderedLogEntry[]>;
+  private lastFetched?: Date;
+  private cachedFarmId?: string;
+
+  constructor(protected authService: AuthService, protected userService: UserService) {}
+
+  public getLogs(farmId: string, limitNumber = 10): Observable<RenderedLogEntry[]> {
+    const isOutdated = this.lastFetched && Date.now() - this.lastFetched.getTime() > 240000;
+    const isDifferentFarm = this.cachedFarmId === farmId;
+    if (isOutdated || isDifferentFarm) this.logsCache$ = undefined;
+    return (this.logsCache$ ||= from(
+      (() => {
+        const ref = collection(Firebase.firestore, farmId, LogService.path);
+        const q = query(ref, limit(limitNumber), orderBy("createdAt", "desc"));
+        return getDocs(q);
+      })(),
+    ).pipe(
+      map((results) => {
+        if (results.empty) return [];
+        return results.docs.map((doc) => doc.data() as LogEntry);
+      }),
+      switchMap((logs) =>
+        forkJoin(
+          logs.map((log) =>
+            this.userService.watchUserDoc(log.uid).pipe(
+              first(),
+              map((userData) => ({ ...log, ...userData })),
+            ),
+          ),
+        ),
+      ),
+      map((logs) =>
+        logs.map<RenderedLogEntry>((log) => ({
+          createdAt: log.createdAt,
+          message: log.displayName ?? log.email + logDictionary[log.action] + log.value,
+        })),
+      ),
+    ));
+  }
+
+  public addLog(farmId: string, actionCode: number, value = "") {
+    return this.authService.getUid().pipe(
+      first(),
+      switchMap((uid) => {
+        const logData: LogEntry = { uid, createdAt: Date.now(), action: actionCode, value };
+        const ref = collection(Firebase.firestore, farmId, LogService.path);
+        return addDoc(ref, logData);
+      }),
+    );
+  }
 }
