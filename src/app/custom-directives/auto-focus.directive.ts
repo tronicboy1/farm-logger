d997986b74b85a381046bc7c0daee3e5c06b7c1b
tronicboy1 <mayeraus@icloud.com>
2022-12-26 20:26:35 +0900

    get auto focus directive working with symbols and universal observer

--- src/app/custom-directives/auto-focus.directive.ts
+++ src/app/custom-directives/auto-focus.directive.ts
@@ -4,9 +4,22 @@ import { AfterViewInit, Directive, ElementRef, inject, OnDestroy } from '@angula
   selector: '[appAutoFocus]',
 })
 export class AutoFocusDirective implements AfterViewInit, OnDestroy {
-  private observer = new IntersectionObserver(
-    (entries) => entries.filter((entry) => entry.isIntersecting).length > 0 && this.nativeElement.focus(),
+  static readonly attributeKey: unique symbol = Symbol('AutoFocusDirectiveAttributeKey');
+  static readonly callbackCache = new Map<symbol, () => void>();
+  static readonly observer = new IntersectionObserver((entries) =>
+    entries
+      .filter((entry) => entry.isIntersecting)
+      .map((entry) => entry.target as HTMLInputElement)
+      .forEach((el) => {
+        const key = el[AutoFocusDirective.attributeKey];
+        if (!key) throw TypeError('Input element has no auto focus attribute key.');
+        const callback = AutoFocusDirective.callbackCache.get(key);
+        if (!callback) throw TypeError('Auto focus callback not cached.');
+        callback();
+      }),
   );
+
+  private uniqueKey = Symbol('InstanceStaticCacheKey');
   private el = inject(ElementRef);
   get nativeElement() {
     if (!(this.el.nativeElement instanceof HTMLInputElement)) throw TypeError();
@@ -14,10 +27,28 @@ export class AutoFocusDirective implements AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit(): void {
-    this.observer.observe(this.nativeElement);
+    this.nativeElement[AutoFocusDirective.attributeKey] = this.uniqueKey;
+    AutoFocusDirective.callbackCache.set(this.uniqueKey, () => this.nativeElement.focus());
+    AutoFocusDirective.observer.observe(this.nativeElement);
   }
 
   ngOnDestroy(): void {
-    this.observer.disconnect();
+    AutoFocusDirective.observer.unobserve(this.nativeElement);
+    AutoFocusDirective.callbackCache.delete(this.uniqueKey);
+  }
+
+  static ngTemplateContextGuard(
+    _directive: AutoFocusDirectiveContext,
+    _context: HTMLInputElement,
+  ): _context is AutoFocusDirectiveContext {
+    return true;
+  }
+}
+
+interface AutoFocusDirectiveContext extends HTMLInputElement {}
+
+declare global {
+  interface HTMLInputElement {
+    [AutoFocusDirective.attributeKey]?: symbol;
   }
 }
