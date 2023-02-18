c84cde31eb06747fafce178fa7201102270b6e84
tronicboy1 <mayeraus@icloud.com>
2023-02-18 15:10:50 +0900

    fix index link for plants

--- src/app/pages/farm/areas/area/index/index.component.ts
+++ src/app/pages/farm/areas/area/index/index.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AreaService } from '@farm/area.service';
 import { LocationArray } from '@tronicboy/ngx-geolocation';
-import { BehaviorSubject, map, mergeMap, tap } from 'rxjs';
+import { BehaviorSubject, map, mergeMap, shareReplay, tap } from 'rxjs';
 import { GeolocationService } from 'src/app/farm/util/geolocation.service';
 import { AreaRouteParamsComponent } from '../route-params.inheritable';
 
@@ -14,7 +15,9 @@ export class AreaIndexComponent extends AreaRouteParamsComponent {
   private geolocationService = inject(GeolocationService);
   readonly area = this.getFarmIdAndAreaId().pipe(
     mergeMap(([farmId, areaId]) => this.areaService.watchArea(farmId, areaId)),
+    shareReplay(1),
   );
+  readonly plantsLink$ = this.area.pipe(AreaService.getPlantsLink);
   readonly areaEditTitle$ = this.area.pipe(map((area) => `${area.name}区域編集`));
   readonly googleMapsURL = this.area.pipe(map((area) => this.geolocationService.getGoogleMapsURL(area)));
   readonly showEditModal$ = new BehaviorSubject(false);
