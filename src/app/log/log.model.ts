7e6383656d573b1eaae50c98fbb5cceed826f87b
tronicboy1 <mayeraus@icloud.com>
2023-02-19 07:13:53 +0900

    convert log record to map

--- src/app/log/log.model.ts
+++ src/app/log/log.model.ts
@@ -39,23 +39,23 @@ export enum LogIconCodes {
   View,
 }
 
-export const logDictionary: Record<LogActions, string> = {
-  [LogActions.CreateFarm]: 'がこの農場を登録した',
-  [LogActions.ViewFarm]: 'がこの農場を閲覧した',
-  [LogActions.ViewArea]: 'が区域を閲覧した：',
-  [LogActions.AddMember]: 'がメンバーを追加した',
-  [LogActions.DeleteMember]: 'がメンバーを削除した',
-  [LogActions.AddArea]: 'が区域を追加した：',
-  [LogActions.AddWeatherData]: 'が気象データを記録した',
-  [LogActions.AddAreaLocation]: 'が区域の位置情報を記録した',
-  [LogActions.AddTree]: 'が木を新たに登録した：',
-  [LogActions.AddTreeReport]: 'が木の育成記録を作成した：',
-  [LogActions.DeleteTreeReport]: 'が木の育成記録を削除した',
-  [LogActions.AddFertilization]: 'が施肥記録を作成した',
-  [LogActions.DeleteFertilization]: 'が施肥記録を削除した',
-  [LogActions.AddCropdust]: 'が散布記録を作成した',
-  [LogActions.DeleteCropdust]: 'が散布記録を削除した',
-  [LogActions.DeleteArea]: 'が区域を削除した：',
-};
+export const logDictionary = new Map<LogActions, string>([
+  [LogActions.CreateFarm, 'がこの農場を登録した'],
+  [LogActions.ViewFarm, 'がこの農場を閲覧した'],
+  [LogActions.ViewArea, 'が区域を閲覧した：'],
+  [LogActions.AddMember, 'がメンバーを追加した'],
+  [LogActions.DeleteMember, 'がメンバーを削除した'],
+  [LogActions.AddArea, 'が区域を追加した：'],
+  [LogActions.AddWeatherData, 'が気象データを記録した'],
+  [LogActions.AddAreaLocation, 'が区域の位置情報を記録した'],
+  [LogActions.AddTree, 'が木を新たに登録した：'],
+  [LogActions.AddTreeReport, 'が木の育成記録を作成した：'],
+  [LogActions.DeleteTreeReport, 'が木の育成記録を削除した'],
+  [LogActions.AddFertilization, 'が施肥記録を作成した'],
+  [LogActions.DeleteFertilization, 'が施肥記録を削除した'],
+  [LogActions.AddCropdust, 'が散布記録を作成した'],
+  [LogActions.DeleteCropdust, 'が散布記録を削除した'],
+  [LogActions.DeleteArea, 'が区域を削除した：'],
+]);
 
 export const viewLogActions = [LogActions.ViewFarm, LogActions.ViewArea];
